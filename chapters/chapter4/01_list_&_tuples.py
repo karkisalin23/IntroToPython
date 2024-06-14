d0828d1e1d23455f49b05041c35c2a0a891be6c0
karki <karkisalin22@yahoo.com>
2024-06-14 15:21:36 -0400

    update

---
+++ chapters/chapter4/01_list_&_tuples.py
@@ -0,0 +1,17 @@
+#List
+"""------------------------------------------"""
+a = [1, 2, 4, 56, 6] #Creating a list using []
+
+print(a)#Print the lising using print() function
+print(a[0]) # to get first index number
+
+a[0] = 98 #replace index 0[1] with 0[98]
+print(a)
+
+#create a list with different data type
+c = [24, "Salin", False, 5.6]
+print(c)
+
+# List slicing
+anime = ["Naruto", "Attack on Titan", "Jujutsu Kaisen", "Classroom of the Elite", "Bleach", "Black Clover", "Dororo", 86] #list of anime 
+print(anime[0:4]) #only print first 4 character/list
